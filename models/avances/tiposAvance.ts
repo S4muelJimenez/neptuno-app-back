346c32cd798ef1ed6a7c91507188a0eedd523828
FervKa <90405746+FervKa@users.noreply.github.com>
2021-11-26 22:54:13 -0500

    Se empieza con los mutation de avance

--- models/avances/tiposAvance.ts
+++ models/avances/tiposAvance.ts
@@ -9,8 +9,21 @@ const typeAvance = gql`
         proyecto: Proyecto!
     }
 
+    input datosAvance{
+        _id: ID!,
+        descripcion:String!
+        estudiante: Usuario!
+        fechaAvance: Date!
+        proyecto: Proyecto!
+    }
+
     type Query{
         Avance(_id:ID): [Avance]
+    }
+
+
+    type Mutation{
+        crearAvance(_id:ID!, campos: datosAvance!):Avance
     }
 
 `;
