e390a07ce5be02ec54ecd64cb8c9ac87dae2cd9b
FervKa <90405746+FervKa@users.noreply.github.com>
2021-12-02 18:05:09 -0500

    Errores culosas

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,8 +1,8 @@
 import {resolversProyecto} from '../models/proyectos/resolversProyecto';
 import {resolversUsuario} from '../models/usuarios/resolversUsuario'
 import { resolversAvances } from '../models/avances/resolversAvance';
-import { resolversIncripcion } from '../models/inscripciones/resolversInscripcion';
+import { resolversInscripcion } from '../models/inscripciones/resolversInscripcion';
 
 
 
-export const resolvers =[resolversProyecto, resolversUsuario, resolversAvances, resolversIncripcion]
+export const resolvers =[resolversProyecto, resolversUsuario, resolversAvances, resolversInscripcion]
