fe267063cc77cca7f6f38f80e06fc702300f7f47
Vicente Velasco <velasco.josevicente@gmail.com>
2021-12-03 20:02:27 -0500

    Se añade linea faltante en resolvers.ts desde graph-JV

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,8 +1,13 @@
-import {resolversProyecto} from '../models/proyectos/resolversProyecto';
-import {resolversUsuario} from '../models/usuarios/resolversUsuario'
-import { resolversAvances } from '../models/avances/resolversAvance';
-import { resolversInscripcion } from '../models/inscripciones/resolversInscripcion';
+import { resolversProyecto } from "../models/proyectos/resolversProyecto";
+import { resolversUsuario } from "../models/usuarios/resolversUsuario";
+import { resolversAvances } from "../models/avances/resolversAvance";
+import { resolversInscripcion } from "../models/inscripciones/resolversInscripcion";
+import { resolversAuth } from "../models/autenticacion/resolversAuth";
 
-
-
-export const resolvers =[resolversProyecto, resolversUsuario, resolversAvances, resolversInscripcion]
+export const resolvers = [
+    resolversProyecto,
+    resolversUsuario,
+    resolversAvances,
+    resolversInscripcion,
+    resolversAuth,
+];
--- graphql/types.ts
+++ graphql/types.ts
@@ -1,13 +1,21 @@
-import { gql } from 'apollo-server-express';
-import { tiposAuth } from '../models/autenticacion/tiposAuth';
-import { typeAvance } from '../models/avances/tiposAvance';
-import { tiposEnums } from '../models/enums/tiposEnums';
-import { tiposProyecto } from '../models/proyectos/tiposProyecto';
-import { tiposUsuario } from '../models/usuarios/tiposUsuario';
-import { tiposInscripcion} from '../models/inscripciones/tiposInscripcion';
+import { gql } from "apollo-server-express";
+import { tiposAuth } from "../models/autenticacion/tiposAuth";
+import { typeAvance } from "../models/avances/tiposAvance";
+import { tiposEnums } from "../models/enums/tiposEnums";
+import { tiposProyecto } from "../models/proyectos/tiposProyecto";
+import { tiposUsuario } from "../models/usuarios/tiposUsuario";
+import { tiposInscripcion } from "../models/inscripciones/tiposInscripcion";
 
 const tipoGobales = gql`
-    scalar Date       
+    scalar Date
 `;
 
-export const typeDefs  = [tipoGobales, tiposEnums, tiposProyecto, tiposUsuario, typeAvance, tiposInscripcion, tiposAuth]
+export const typeDefs = [
+    tipoGobales,
+    tiposEnums,
+    tiposProyecto,
+    tiposUsuario,
+    typeAvance,
+    tiposInscripcion,
+    tiposAuth,
+];
